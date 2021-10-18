236c5fa2f260015279fd67c1f6ae93d07457282a
Mauricio Paternina <spaceinvadev@icloud.com>
2021-10-18 17:28:18 -0500

    add path resolution to tsconfig.json

--- cartagena-plans/server/src/database/index.ts
+++ cartagena-plans/server/src/database/index.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from 'mongodb'
-import { Database } from '../lib/types'
+import { Database } from 'types'
 
 const URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_URL}/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
